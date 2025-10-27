e01d19a5335c2079d803d6cabacc8fc50889681e
romancumayev099 <roman.cumayev099@gmail.com>
2025-10-27 21:41:12 +0400

    .
    .

--- polls/views.py
+++ polls/views.py
@@ -1,39 +1,57 @@
 # polls/views.py
 
-# Çatışmayan funksiyaları və modelləri import edirik
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse # HttpResponseRedirect və HttpResponse-u əlavə etdik
-from django.urls import reverse # reverse funksiyasını əlavə etdik
-from django.db.models import F # F ifadəsini əlavə etdik
-
-# Question modelini import etmişdik, Choice modelini də əlavə edirik (Çünki vote funksiyasında istifadə olunur)
+from django.http import HttpResponseRedirect, HttpResponse, Http404 # ❗ Http404 əlavə edildi
+from django.urls import reverse
+from django.db.models import F 
+from django.utils import timezone
 from .models import Question, Choice 
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")[:5]
     
-    # OUTPUT-u (çıxışı) sadə mətn əvəzinə, şablon vasitəsilə göndəririk
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
     
+    
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    # Həll 2 üçün 'polls/detail.html' faylının mövcud olduğunu yoxlayın
+    # ❗ DÜZƏLİŞ: Obyekti əldə etməzdən əvvəl pub_date-i yoxlayırıq.
+    try:
+        question = Question.objects.filter(
+            pub_date__lte=timezone.now(), # Yalnız keçmiş sualları qəbul edir
+            pk=question_id,
+        ).get() # Əgər tapılmasa, DoesNotExist xətası verir.
+    except Question.DoesNotExist:
+        # Gələcək sual tapılsa (və ya ümumiyyətlə tapılmasa) 404 qaytarır.
+        raise Http404("Question does not exist") 
+
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    # Düzəliş: Sondakı əlavə mötərizəni sildik.
-    return render(request, "polls/results.html", {"question": question}) 
+    # ❗ DÜZƏLİŞ: results view-u üçün də eyni filtrləməni tətbiq edirik.
+    try:
+        question = Question.objects.filter(
+            pub_date__lte=timezone.now(),
+            pk=question_id,
+        ).get()
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+        
+    return render(request, "polls/results.html", {"question": question})  
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    # Burada filtrləməyə ehtiyac yoxdur, çünki istifadəçi 'detail' səhifəsində səs verir.
+    # Lakin, dərsliyin məntiqini tamamlamaq üçün Part 6-da bu hissəyə qayıdıb
+    # 'was_published_recently' istifadə edə bilərik. İndilik keçək.
     try:
-        # Seçim id-si (pk) POST data vasitəsilə gəlir
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        # Səs verilmədiyi halda xəta mesajı ilə 'detail' şablonuna qayıdırıq.
         return render(
             request,
             "polls/detail.html",
@@ -43,8 +61,6 @@ def vote(request, question_id):
             },
         )
     else:
-        # F("votes") + 1 atomar bir əməliyyat üçün istifadə olunur
-        selected_choice.votes = F("votes") + 1 
+        selected_choice.votes = F("votes") + 1  
         selected_choice.save()
-        # POST datası uğurla idarə edildikdən sonra həmişə HttpResponseRedirect qaytarın.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
